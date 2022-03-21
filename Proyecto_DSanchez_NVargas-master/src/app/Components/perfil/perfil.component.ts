93bf530575bd05345a38b304463d5aedc8384dd2
Nicolas Daniel Vargas Ortiz <nicroleto1971@gmail.com>
2022-03-21 14:37:23 -0500

    commit

--- Proyecto_DSanchez_NVargas-master/src/app/Components/perfil/perfil.component.ts
+++ Proyecto_DSanchez_NVargas-master/src/app/Components/perfil/perfil.component.ts
@@ -11,6 +11,8 @@ import { ClienteService } from 'src/app/servicios/clientes.service';
 export class PerfilComponent implements OnInit {
 
   public usu:cliente;
+  public numImg:number=3;
+  public imgs:string[]=["../../../assets/images/userIcons/1.png","../../../assets/images/userIcons/2.png","../../../assets/images/userIcons/3.png"];
 
   constructor(public router:Router,public _clienteService: ClienteService) { 
     this.usu = _clienteService.actual;
@@ -21,6 +23,12 @@ export class PerfilComponent implements OnInit {
 
   verFacturas(){
     this.router.navigateByUrl("./verFacts")
+  }
+
+  tomarImg():string{
+    var num : number = Math.floor(Math.random() * this.numImg);
+    var Simg:string = this.imgs[num];
+    return Simg;
   }
   
 }
